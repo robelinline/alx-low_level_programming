347c1cd8b30f6c251a086cc15867ec5da68d111d
robelinline <robelamare0@gmail.com>
2023-06-20 13:54:54 -0700

    done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * add - adds two integrers 
  * @x: The first integer to be added.
  * @y: The second integer to be added.
  * Return: Always 0.
- *
  */
 int add(int x, int y)
 {
