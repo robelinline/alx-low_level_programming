cca542309723a3d6f7bfd624cc0d99470d2ccaae
robelinline <robelamare0@gmail.com>
2023-06-20 12:55:31 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
+void print_to_98(int n);
