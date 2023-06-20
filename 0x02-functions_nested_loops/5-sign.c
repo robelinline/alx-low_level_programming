fff47ce9b0beeeb73d20477f0493c4403e619f0b
robelinline <robelamare0@gmail.com>
2023-06-20 13:44:28 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,9 @@
 /**
  * print_sign - prints the sign of a numvera
  * @n: The number of which the sign will be printed.
- * Return -1 and prints - if n is less than zero
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
