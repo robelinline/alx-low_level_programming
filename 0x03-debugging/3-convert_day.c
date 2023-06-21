4c0650be08c77536a3d2ca52570f4c20625746c8
robelinline <robelamare0@gmail.com>
2023-06-21 07:07:14 -0700

    repair

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -50,15 +50,6 @@ int convert_day(int month, int day)
     }
     return (day);
 }
-#include "main.h"
-
-/**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
-*/
 
 int convert_day(int month, int day)
 {
