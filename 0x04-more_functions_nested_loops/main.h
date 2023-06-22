5eb5fc04ffb1ed553a3aaac171b77bb9b9297997
robelinline <robelamare0@gmail.com>
2023-06-22 11:22:55 -0700

    errors

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
- */
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
