7a10bce1089255769bccb71478f6ad20aad95ee7
robelinline <robelamare0@gmail.com>
2023-06-26 10:34:15 -0700

    files being added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_array - function that  followed by a new line.
+ * @a: This is the input array
+ * @n: This is the lenght of the array
+ *
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+	
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		if (index != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme for strings and pointers class
