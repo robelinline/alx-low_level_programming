ef951e903515c64071bed50a0eefdaa700180d14
robelinline <robelamare0@gmail.com>
2023-06-30 11:52:52 -0700

    reverse array error

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,30 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * reverse_array - reverse array
- * @a:array
- * @n:integer
- * Return:void
- */
+  * reverse_array - Reverses the integers
+  * @a: int
+  * @n: swap
+  *
+  * Return: empty
+  */
 void reverse_array(int *a, int n)
 {
-	int i, c;
-	for (i = 0; (i < (n - 1) / 2); i++)
+	int *p, i, aux, k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
 	{
-		c = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = c;
+		p++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
 }
+
