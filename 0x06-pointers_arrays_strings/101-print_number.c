27b1d1c30fd7da9152ef0759829f815060c0ef63
robelinline <robelamare0@gmail.com>
2023-06-30 12:02:52 -0700

    print numbers putchar  solved

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,17 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int x;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		n *= -1;
 	}
-	if ((x / 10) > 0)
+
+	x = n;
+
+	if (x / 10)
 		print_number(x / 10);
 
 	_putchar(x % 10 + '0');
