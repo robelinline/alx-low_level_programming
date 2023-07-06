5abfa00b580ea233f68f29bfbcb0375e8fadce4e
robelinline <robelamare0@gmail.com>
2023-07-06 11:43:51 -0700

    solve third q

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
  * _pow_recursion - Write a function that ret
- * power of y.
- *
+ * power of y
  * @x: This is the input number
  * @y: This is the power number
- *
- * Return: if "y" is bigger than zero raised to the power of "y",
- * return -1 if "y" is lower than zero, return 1 if "y" is equal to 1, 
+ * Return: if "y" is bigger than zero raised to the power of "y"
+ * return -1 if "y" is lower than zero, return 1 if "y" is equal to
  */
 int _pow_recursion(int x, int y)
 {
