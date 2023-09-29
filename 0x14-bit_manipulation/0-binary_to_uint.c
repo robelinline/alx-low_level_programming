bf3f2b0521c91c15c983b664f01d5e36a36b5a73
robelinline <62613359+robelinline@users.noreply.github.com>
2023-09-29 22:03:55 +0300

    Update 0-binary_to_uint.c betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,25 +11,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 		signed int index = 0, num = 0;
-
-
 			if (!b)
-						return (0);
-
-				while (b[index] != '\0')
-						{
-									if (b[index] != '0' && b[index] != '1')
-												{
-																return (0);
-																		}
-
-											num <<= 1;
-
-													if (b[index] & 1)
-																{
-																				num += 1;
-																						}
-															index += 1;
-																}
-					return (num);
+				return (0);
+	while (b[index] != '\0')
+	{
+		if (b[index] != '0' && b[index] != '1')
+		{
+			return (0);
+		}
+		num <<= 1;
+		if (b[index] & 1)
+		{
+			num += 1;
+		}
+		index += 1;
+	}
+	return (num);
 }
