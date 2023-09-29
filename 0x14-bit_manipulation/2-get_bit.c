b4f398b54020ac65a6f0a5114637b1a65d20890e
robelinline <62613359+robelinline@users.noreply.github.com>
2023-09-29 22:05:10 +0300

    betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,29 +1,29 @@
-nclude "main.h"
+#include "holberton.h"
 
 /**
- *  * get_bit - Get value of a bit at a given index
- *   *
- *    * @n: long int
- *     *
- *      * @index: unsigned int
- *       *
- *        * Return: Bit at index
- *         */
+ * get_bit - Get value of a bit at a given index
+ *
+ * @n: long int
+ *
+ * @index: unsigned int
+ *
+ * Return: Bit at index
+ */
 
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-		unsigned long int mask = 1;
+	unsigned long int mask = 1;
 
-			if (index > sizeof(n) * 8)
-					{
-								return (-1);
-									}
+	if (index > sizeof(n) * 8)
+	{
+		return (-1);
+	}
 
-				mask <<= index;
+	mask <<= index;
 
-					if (mask & n)
-								return (1);
-						else
-									return (0);
+	if (mask & n)
+		return (1);
+	else
+		return (0);
 }
